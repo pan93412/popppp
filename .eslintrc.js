09b7df12a45f80345c02915e713b4178e08273a0
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-18 21:20:28 +0800

    init: prepare linters

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+    extends: [
+      "eslint:recommended",
+      'plugin:vue/vue3-recommended',
+      'plugin:import/recommended',
+      'plugin:import/typescript',
+      "plugin:@typescript-eslint/recommended",
+      "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      'airbnb-typescript/base',
+      "plugin:prettier/recommended",
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: 'module',
+      project: "./tsconfig.json",
+    },
+    plugins: [
+      'react',
+      '@typescript-eslint',
+    ],
+    rules: {
+      "@typescript-eslint/consistent-type-imports": [
+        2,
+        {
+          prefer: "type-imports"
+        },
+      ],
+      "@typescript-eslint/explicit-module-boundary-types": 0,
+      "@typescript-eslint/restrict-template-expressions": 0,
+      "import/order": [
+        1,
+        {
+          groups: ["builtin", "external", "internal", "parent", "sibling", "index", "object"],
+        },
+      ],
+      "arrow-body-style": 2,
+      "no-void": ["error", { "allowAsStatement": true }],
+    },
+  };
